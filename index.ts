3d49a42c7626dcfc351e650d44ecdc6ef07e5770
Arnaud <arnaudjourdain@orange.fr>
2022-04-21 22:05:48 +0200

    make it better

---
+++ index.ts
@@ -0,0 +1,23 @@
+import {config} from "dotenv";
+config();
+
+import express from "express";
+import mongoose, {Mongoose} from "mongoose";
+
+async function startServer(): Promise<void> {
+
+    const m: Mongoose = await mongoose.connect(process.env.MONGO_URI as string, {
+        auth: {
+            username: process.env.MONGO_USER  as string,
+            password: process.env.MONGO_PASSWORD as string
+        }
+    });
+
+    const app = express();
+
+    app.listen(process.env.PORT, function() {
+        console.log("Server listening on port " + process.env.PORT);
+    });
+}
+
+startServer().catch(console.error);
